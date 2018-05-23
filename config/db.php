966cba123acc4ef460c59810089d1cd82effa448
lentrix <gijhaycosmod123@gmail.com>
2018-05-23 11:19:47 +0800

    updated

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ws_lemuel',
+    'username' => 'ws_lemuel',
+    'password' => 'lemuel6871',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
