e589a39bd4e33705448d2a26985387f5df4eae0a
bhillyjohn <gijhaycosmod123@gmail.com>
2018-05-24 15:36:23 +0800

    Last Update na to promise

--- views/producer/update.php
+++ views/producer/update.php
@@ -1,5 +1,8 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use app\models\Music;
+use app\models\Artist;
 use yii\widgets\ActiveForm;
 ?>
 <h1>Update Your Producer</h1>
@@ -9,9 +12,11 @@ use yii\widgets\ActiveForm;
 
 		<?php $form = ActiveForm::begin() ?>
 
-            <?= $form->field($model, 'music_id')->textInput() ?>
+            <?= $form->field($model,'music_id')->dropDownList(ArrayHelper::map(
+				Music::find()->asArray()->all(), 'id','title',''))?>
         
-			<?= $form->field($model, 'artist_id')->textInput() ?>
+			<?= $form->field($model,'artist_id')->dropDownList(ArrayHelper::map(
+				Artist::find()->asArray()->all(), 'id','name'))?>
 
 			<?= $form->field($model, 'producer_name')->textInput() ?>
 
